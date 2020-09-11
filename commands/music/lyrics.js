b6c20d5e474ba915ff55ed29998b9d6cfdf0cdfb
DeathHound6 <acraig176@gmail.com>
2020-09-11 19:39:16 +0100

    Update lyrics.js

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -1,3 +1,27 @@
-module.exports = {
+const { MessageEmbed, Util } = require("discord.js");
+const lyrics = require("solenolyrics");
 
+module.exports = {
+  help: {
+    name: "lyrics",
+  },
+  run: async (message, args) => {
+    if(!args[0]) return message.reply("You must provide a Search Query");
+    let lyr = await lyrics.requestLyricsFor(args.slice(0).join(" "));
+    let auth = await lyrics.requestAuthorFor(args.slice(0).join(" "));
+    let title = await lyrics.requestTitleFor(args.slice(0).join(" "));
+    let icon = await lyrics.requestIconFor(args.slice(0).join(" "));
+
+    let arr = Util.splitMessage(lyr, { maxLength: 2048, char: "\n" });
+    for(let i = 0; i < arr.length; i++){
+      let embed = new MessageEmbed({
+        description: arr[i],
+        color: "RANDOM",
+        title: `**${title}** by **${auth}**`,
+        timestamp: Date.now(),
+        thumbnail: icon
+      });
+      message.channel.send({ embed });
+    }
+  }
 }

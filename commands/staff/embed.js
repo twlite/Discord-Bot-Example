a87cd7a609c8da25f7e78714a2f52c52e20a95b0
FlaringPhoenix <joshmeubanks@gmail.com>
2020-09-11 14:48:18 -0500

    Create embed.js

---
+++ commands/staff/embed.js
@@ -0,0 +1,20 @@
+exports.run = async (client, message, args, guildConf) => {
+
+    let msg = args.join(' ');
+
+    if (!client.checkPerms(message)) {await client.sendErrorEmbed(`Insufficient Permissions`); return;}
+    if (!msg) {await client.sendErrorEmbed(message.channel, 'Please provide a message to embed'); return;}
+
+    await message.delete();
+    await client.sendEmbed(message.channel, "", args.join("").slice(0, 1024), "", `From: ${message.author.username}`)
+
+    return;
+
+};
+
+module.exports.help = {
+    name: "embed",
+    description: "Makes the bot send an embed",
+    dm: false,
+    aliases: ["say"]
+}

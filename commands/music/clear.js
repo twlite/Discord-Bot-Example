568ad01809ceff3cc532b5c5a2d5d164ebc02912
TheGamerYT <37266659+TheGamer456YT@users.noreply.github.com>
2020-09-12 21:44:17 +0100

    Updated clear.js

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -1,33 +1,45 @@
-module.exports = {
-    help: {
-        name: "clear",
-		description: "Clear's the queue",
-		category: "Music"
-    },
-    run: async (client, message, args) => {
+exports.run = async (client, message, args) => {
         
-        if (!message.member.voice.channel) return message.channel.send({ embed: {
-            description: `❌ | You are not in a voice channel!`,
-            color: 0xff0000
-        }});
+        const embederrorNotInVC = new Discord.MessageEmbed()
+	 .setFooter(client.config.embed.footer)
+	 .setColor(client.config.embed.color)
+	 .setDescription(`❌ | You are not in a voice channel!`);
+	 
+	    
+        if (!message.member.voice.channel) return message.channel.send(embederrorNotInVC);
         
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send({ embed: {
-            description: `❌ | You are not in my voice channel!`,
-            color: 0xff0000
-        }});
+	const embederrorNotInMyVC = new Discord.MessageEmbed()
+	 .setFooter(client.config.embed.footer)
+	 .setColor(client.config.embed.color)
+	 .setDescription(`❌ | You are not in my voice channel!`);
+	
+	
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(embederrorNotInMyVC);
         
+	const embederrorNotPlaying = new Discord.MessageEmbed()
+	 .setFooter(client.config.embed.footer)
+	 .setColor(client.config.embed.color)
+	 .setDescription(`❌ | I'm not playing anything!`);
+	
+	
         if (!client.player.isPlaying(message.guild.id)) {
-            return message.channel.send({ embed: {
-                description: `❌ | I'm not playing anything!`,
-                color: 0xff0000
-            }});
+            return message.channel.send(embederrorNotPlaying);
         }
         
         await client.player.clearQueue(message.guild.id);
         
-        return message.channel.send({ embed: {
-            description: `✅ | Queue cleared!`,
-            color: 0x3498db
-        }});
+	const embedSuccess = new Discord.MessageEmbed()
+	 .setFooter(client.config.embed.footer)
+	 .setColor(client.config.embed.color)
+	 .setDescription(`✅ | Queue cleared!`);
+	
+        return message.channel.send(embedSuccess);
     }
 };
+
+module.exports.help = {
+    name: "clear",
+    description: "Clear's the queue",
+    dm: false,
+    aliases: []
+}

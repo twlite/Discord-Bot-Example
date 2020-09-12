95ecee2c8fc4c61a9adc0895894f99bc66ed9540
TheGamerYT <37266659+TheGamer456YT@users.noreply.github.com>
2020-09-12 12:58:50 +0100

    Create reload.js

---
+++ commands/owner/reload.js
@@ -0,0 +1,39 @@
+const discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+
+    if (!client.isOwner(message.author.id)) { return; }
+
+    if (!args[0]) return message.channel.send("Please provide a command to reload!");
+
+    const commandName = args[0].toLowerCase();
+	
+    const command = client.commands.get(commandName) || client.commands.get(cient.aliases.get(commandName));
+	
+    if (!command) return message.channel.send("That command doesn't exist. Try again.");
+	
+	  readdirSync(join(__dirname, "..")).forEach(f => {
+		  const files = readdirSync(join(__dirname, "..", f));
+		  if (files.includes(`${commandName}.js`)) {
+			  const file = `../${f}/${commandName}.js`;
+			  try {
+			  	delete require.cache[require.resolve(file)];
+			  	client.commands.delete(commandName);
+			  	const pull = require(file);
+			  	client.commands.set(commandName, pull);
+			  	return message.channel.send(`Successfully reloaded \`${commandName}.js\` !`);
+		  	}
+			  catch (err) {
+			  	message.channel.send(`Could not reload: \`${args[0].toUpperCase()}\` `);
+		  		return console.log(err.stack || err);
+        }
+		  }
+	  })
+};
+
+module.exports.help = {
+  name: "reload",
+  description: "Reload's a command",
+  dm: true,
+  aliases: ["r"]
+}

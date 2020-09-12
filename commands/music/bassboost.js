6f260bb895ade4e827a3a04bf4ec324ebbd82861
TheGamerYT <37266659+TheGamer456YT@users.noreply.github.com>
2020-09-12 21:41:46 +0100

    Updated bassboost.js

--- commands/music/bassboost.js
+++ commands/music/bassboost.js
@@ -1,35 +1,50 @@
-module.exports = {
-    help: {
-        name: "bassboost",
-		description: "Bassboost the current song",
-        aliases: ["bass"],
-		category: "Music"
-    },
-    run: async (client, message, args) => {
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
         
-        if (!message.member.voice.channel) return message.channel.send({ embed: {
-            description: `❌ | You are not in a voice channel!`,
-            color: 0xff0000
-        }});
+        const embederrorNotInVC = new Discord.MessageEmbed()
+	 .setFooter(client.config.embed.footer)
+	 .setColor(client.config.embed.color)
+	 .setDescription(`❌ | You are not in a voice channel!`);
+	 
+	    
+        if (!message.member.voice.channel) return message.channel.send(embederrorNotInVC);
         
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send({ embed: {
-            description: `❌ | You are not in my voice channel!`,
-            color: 0xff0000
-        }});
+	const embederrorNotInMyVC = new Discord.MessageEmbed()
+	 .setFooter(client.config.embed.footer)
+	 .setColor(client.config.embed.color)
+	 .setDescription(`❌ | You are not in my voice channel!`);
+	
+	
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(embederrorNotInMyVC);
         
+	const embederrorNotPlaying = new Discord.MessageEmbed()
+	 .setFooter(client.config.embed.footer)
+	 .setColor(client.config.embed.color)
+	 .setDescription(`❌ | I'm not playing anything!`);
+	
+	
         if (!client.player.isPlaying(message.guild.id)) {
-            return message.channel.send({ embed: {
-                description: `❌ | I'm not playing anything!`,
-                color: 0xff0000
-            }});
+            return message.channel.send(embederrorNotPlaying);
         }
+	
         const bassboostEnabled = await client.player.getQueue(message.guild.id).filters.bassboost;
         client.player.setFilters(message.guild.id, {
             bassboost: !bassboostEnabled
         });
-        return message.channel.send({ embed: {
-            description: `✅ | Bassboost ${!bassboostEnabled ? "Enabled" : "Disabled"}!`,
-            color: 0x3498db
-        }})
+	
+	const embedSuccess = new Discord.MessageEmbed()
+	 .setFooter(client.config.embed.footer)
+	 .setColor(client.config.embed.color)
+	 .setDescription(`✅ | Bassboost ${!bassboostEnabled ? "Enabled" : "Disabled"}!`)
+	
+        return message.channel.send(embedSuccess)
     }
 };
+
+module.exports.help = {
+    name: "bassboost",
+    description: "Add's the bassboost sound effect to the playing music",
+    dm: false,
+    aliases: ["bass"]
+}
